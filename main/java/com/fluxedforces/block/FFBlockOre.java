e71a51fd923d96b135bc9d185fe80451bedeffa9
tim4242 <tim4242@gmx.de>
2015-02-20 20:11:27 +0100

    Update

--- main/java/com/fluxedforces/block/FFBlockOre.java
+++ main/java/com/fluxedforces/block/FFBlockOre.java
@@ -3,8 +3,13 @@
  */
 package com.fluxedforces.block;
 
+import com.fluxedforces.help.FFUtil;
+
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 /**
  * @author tim4242, kaigan54, TheCrimsonSpark
@@ -12,18 +17,23 @@ import net.minecraft.item.Item;
  */
 public class FFBlockOre extends FFBlock {
 
-	private static final String[] m_names = new String[] {};
-	private static final Item[] m_drops = new Item[] {};
-	private static final int[] m_dropNumbers = new int[] {};
-	private static final String[] m_unlocNames = new String[] {};
+	private static final ItemStack[] m_drops = new ItemStack[] {};
+	private static final int[] m_dropNumbers = new int[] {1};
+	private static final String[] m_unlocNames = new String[] {""};
 	
 	private int m_oreType;
 	
 	public FFBlockOre(int oreType)
 	{
-		super(m_unlocNames[oreType], m_unlocNames[oreType], Material.rock);
+		super("ore/" + m_unlocNames[oreType], m_unlocNames[oreType], Material.rock);
 		
 		this.m_oreType = oreType;
+	}
+	
+	public void onBlockHarvested(World world, int x, int y, int z, int meta, EntityPlayer player)
+	{
+		for(int i = 0; i < m_dropNumbers[m_oreType]; i++)
+			FFUtil.dropBlock(m_drops[m_oreType], world, x, y, z, player);
 	}
 	
 }
--- main/java/com/fluxedforces/help/FFUtil.java
+++ main/java/com/fluxedforces/help/FFUtil.java
@@ -3,7 +3,11 @@
  */
 package com.fluxedforces.help;
 
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.StatCollector;
+import net.minecraft.world.World;
 
 /**
  * @author tim4242, kaigan54, TheCrimsonSpark
@@ -19,6 +23,21 @@ public class FFUtil {
 	public static boolean isValString(String str)
 	{
 		return (!str.equals("") && str != null);
+	}
+	
+	public static void dropBlock(ItemStack stack, World world, float x, float y, float z, EntityPlayer player)
+	{
+		  if (!world.isRemote && world.getGameRules().getGameRuleBooleanValue("doTileDrops") && !world.restoringBlockSnapshots)
+	        {
+	           
+	            float f = 0.7F;
+	            double d0 = (double)(world.rand.nextFloat() * f) + (double)(1.0F - f) * 0.5D;
+	            double d1 = (double)(world.rand.nextFloat() * f) + (double)(1.0F - f) * 0.5D;
+	            double d2 = (double)(world.rand.nextFloat() * f) + (double)(1.0F - f) * 0.5D;
+	            EntityItem entityitem = new EntityItem(world, (double)x + d0, (double)y + d1, (double)z + d2, stack);
+	            entityitem.delayBeforeCanPickup = 10;
+	            world.spawnEntityInWorld(entityitem);
+	        }
 	}
 	
 }
