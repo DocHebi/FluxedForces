13e3eb3d766f9b999ec8802b94c96cd4a0ee9927
tim4242 <tim4242@gmx.de>
2015-02-20 20:37:01 +0100

    Robot

---
+++ main/java/com/fluxedforces/robot/FFRobot.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package com.fluxedforces.robot;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+
+/**
+ * @author tim4242, kaigan54, TheCrimsonSpark
+ *
+ */
+public class FFRobot extends Entity {
+
+	/**
+	 * @param world
+	 */
+	public FFRobot(World world) {
+		super(world);
+	}
+
+	/* (non-Javadoc)
+	 * @see net.minecraft.entity.Entity#entityInit()
+	 */
+	@Override
+	protected void entityInit() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see net.minecraft.entity.Entity#readEntityFromNBT(net.minecraft.nbt.NBTTagCompound)
+	 */
+	@Override
+	protected void readEntityFromNBT(NBTTagCompound p_70037_1_) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see net.minecraft.entity.Entity#writeEntityToNBT(net.minecraft.nbt.NBTTagCompound)
+	 */
+	@Override
+	protected void writeEntityToNBT(NBTTagCompound p_70014_1_) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
