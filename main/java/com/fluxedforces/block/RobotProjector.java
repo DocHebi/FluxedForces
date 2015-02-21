7073a4363522f6b84496b50437658b4b07e3cacb
tim4242 <tim4242@gmx.de>
2015-02-22 00:03:49 +0100

    Update

--- main/java/com/fluxedforces/block/RobotProjector.java
+++ main/java/com/fluxedforces/block/RobotProjector.java
@@ -7,6 +7,7 @@ import com.fluxedforces.robot.FFRobot;
 
 import cpw.mods.fml.common.registry.EntityRegistry;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.world.World;
 
 /**
@@ -15,22 +16,33 @@ import net.minecraft.world.World;
  */
 public class RobotProjector extends FFBlock
 {
+	
+	private FFRobot robot;
 
 	/**
 	 * @param unlocName
 	 * @param textureName
 	 * @param material
 	 */
-	protected RobotProjector()
+	public RobotProjector()
 	{
 		super("projector", "projector", Material.iron);
 	}
 	
 	public int onBlockPlaced(World world, int x, int y, int z, int meta, float f1, float f2, float f3, int i1)
 	{
-		world.spawnEntityInWorld(new FFRobot(world));
+		robot = new FFRobot(world);
+		robot.setPosition(x, y + 1, z);
+		
+		world.spawnEntityInWorld(robot);
 		
 		return i1;
+	}
+	
+	@Override
+	public void onBlockPreDestroy(World world, int x, int y, int z, int meta)
+	{
+		world.removeEntity(robot);
 	}
 
 }
--- main/java/com/fluxedforces/init/FFBlocks.java
+++ main/java/com/fluxedforces/init/FFBlocks.java
@@ -1,19 +1,26 @@
 package com.fluxedforces.init;
 
+import com.fluxedforces.block.RobotProjector;
+import com.fluxedforces.help.RegistryHelper;
+
 /**
  * @author tim4242, kaigan54, TheCrimsonSpark
  *
  */
 public class FFBlocks{
 
+	public static RobotProjector RobotProjector;
 	
 	public static boolean init() {
+		
+		RobotProjector = new RobotProjector();
+		
 		return false;
 	}
 	
 	public static void registerBlocks()
 	{
-		
+		RegistryHelper.registerBlock(RobotProjector);
 	}
 
 }
