455fa314d255a048c2116beaac46b2b66fbc0407
tim4242 <tim4242@gmx.de>
2015-02-22 00:06:17 +0100

    Update

--- main/java/com/fluxedforces/model/render/RobotRenderer.java
+++ main/java/com/fluxedforces/model/render/RobotRenderer.java
@@ -7,6 +7,7 @@ import com.fluxedforces.model.RobotModel;
 import com.fluxedforces.robot.FFRobot;
 
 import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RendererLivingEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
@@ -15,28 +16,29 @@ import net.minecraft.util.ResourceLocation;
  * @author tim4242, kaigan54, TheCrimsonSpark
  *
  */
-public class RobotRenderer extends RendererLivingEntity {
+public class RobotRenderer extends Render {
 
-	/**
-	 * @param model
-	 * @param shadow
+	/* (non-Javadoc)
+	 * @see net.minecraft.client.renderer.entity.Render#doRender(net.minecraft.entity.Entity, double, double, double, float, float)
 	 */
-	public RobotRenderer() 
+	@Override
+	public void doRender(Entity p_76986_1_, double p_76986_2_,
+			double p_76986_4_, double p_76986_6_, float p_76986_8_,
+			float p_76986_9_)
 	{
-		super(new RobotModel(), 0.5f);
+		// TODO Auto-generated method stub
+		
 	}
 
 	/* (non-Javadoc)
 	 * @see net.minecraft.client.renderer.entity.Render#getEntityTexture(net.minecraft.entity.Entity)
 	 */
 	@Override
-	protected ResourceLocation getEntityTexture(Entity entity) 
+	protected ResourceLocation getEntityTexture(Entity entity)
 	{
-		if(entity instanceof FFRobot)
-		{
-			return (ResourceLocation) ((FFRobot) entity).m_texture;
-		}
-		return null;
+		return ((FFRobot) entity).m_texture;
 	}
+
+
 
 }
